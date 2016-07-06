6b6c06392d09b2fa3e085a9844be26d456dd3a56
toshiemon18 <st12318@gmail.com>
2016-07-07 01:00:16 +0900

    Draw only hidden layer outputs

--- autoencoder.py
+++ autoencoder.py
@@ -19,8 +19,9 @@ def draw_digit(data, row, col, n):
     size = int(np.sqrt(data.shape[0]))
     plt.subplot(row, col, n)
     plt.imshow(data.reshape(size, size))
+    plt.gray()
 
-epochs = 10
+epochs = 2
 batch_size = 258
 input_dim = 28
 input_unit_size = input_dim ** 2
@@ -34,7 +35,7 @@ inputs = Input(shape=(input_unit_size,))
 x = Dense(144, activation='relu')(inputs)
 outputs = Dense(input_unit_size)(x)
 model = Model(input=inputs, output=outputs)
-model.compile(loss='mse', optimizer='adadelta')
+model.compile(loss='mse', optimizer='adam')
 
 # トレーニング
 early_stopping = EarlyStopping(monitor='loss', patience=5)
@@ -43,26 +44,26 @@ model.fit(X_train, X_train, callbacks=[early_stopping],
 
 # オリジナルの入力データを描画
 show_size = 10
-total = 0
-plt.figure(figsize=(20, 20))
-for i in range(show_size):
-    for j in range(show_size):
-        draw_digit(X_train[total], show_size, show_size, total+1)
-        total += 1
-plt.show()
+# total = 0
+# plt.figure(figsize=(20, 20))
+# for i in range(show_size):
+#     for j in range(show_size):
+#         draw_digit(X_train[total], show_size, show_size, total+1)
+#         total += 1
+# plt.show()
 
 # 隠れ層の学習状況を描画
-get_layer_output = K.function([model.layers[0].input],
-                              [model.layers[1].output])
-hidden_outputs = get_layer_output([X_train[0:show_size**2]])[0]
-
-total = 0
-plt.figure(figsize=(20, 20))
-for i in range(show_size):
-    for j in range(show_size):
-        draw_digit(hidden_outputs[total], show_size, show_size, total+1)
-        total+=1
-plt.show()
+# get_layer_output = K.function([model.layers[0].input],
+#                               [model.layers[1].output])
+# hidden_outputs = get_layer_output([X_train[0:show_size**2]])[0]
+#
+# total = 0
+# plt.figure(figsize=(20, 20))
+# for i in range(show_size):
+#     for j in range(show_size):
+#         draw_digit(hidden_outputs[total], show_size, show_size, total+1)
+#         total+=1
+# plt.show()
 
 # デコードした出力層の出力を描画
 get_layer_output = K.function([model.layers[0].input],
